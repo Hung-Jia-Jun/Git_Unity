36f4683aee1d2e4ca86d647d254f83530d0445b3
yf9000555 <yf9000555@gmail.com>
2016-05-21 04:38:03 +0800

    增加Tag 1-6的偵測狀態

---
+++ ChangeScene.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+public class ChangeScene: MonoBehaviour
+{
+	Change_Scene_Class Change_This_Scene = new Change_Scene_Class ();
+	public int Player_Mode_Switch;
+	int Mode_Control;
+	int Mode_Sound_Str;
+	int Mode_Scenes_Deside;
+	int Mode_Language_Deside;
+	void Start ()
+	{
+		Player_Setting_Mode();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+		switch (Player_Mode_Switch)
+		{
+			case 0:
+			{
+				break;
+			}
+			case 1:
+			{
+				break;
+			}
+			case 2:
+			{
+				break;
+			}
+			case 3:
+			{
+				break;
+			}
+		}
+	}
+
+public void Player_Setting_Mode()
+{
+	Mode_Control=Change_This_Scene.Control_Deside();//判斷Player_Setting裡面的控制方式狀態，並回傳數值
+	Debug.Log(Mode_Control);
+
+	Mode_Sound_Str=Change_This_Scene.Sound_Str_Content();//判斷Player_Setting裡面的劇本填入狀態，並回傳數值
+	Debug.Log(Mode_Sound_Str);
+
+	Mode_Scenes_Deside=Change_This_Scene.Scenes_Deside();//判斷Player_Setting裡面的場景選擇狀態，並回傳數值
+	Debug.Log(Mode_Scenes_Deside);
+
+	Mode_Language_Deside=Change_This_Scene.Language_Deside();//判斷Player_Setting裡面的語言選擇狀態，並回傳數值
+	Debug.Log(Mode_Language_Deside);
+
+}
+
+
+
+
+
+}
+
+
---
+++ Change_Scene_Class.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+public class Change_Scene_Class {
+	public string ScenceName;//可填入場景名稱
+	int Control;
+	int int_Sound_Str;
+	int int_Scenes_Deside;
+	int int_Language_Deside;
+	public void ChangeScence(string ScenceName)
+	{
+		Application.LoadLevel(ScenceName);
+	}
+	public string ReadFile(string FileName,int linenumber)
+	{
+		System.Text.Encoding.GetEncoding ("utf-8");
+		string[] strs=File.ReadAllLines(FileName,System.Text.Encoding.Default);
+		if (linenumber==0)
+		{
+			return "";
+		}
+		else
+		{
+			return strs[linenumber-1];
+		}
+	}
+	public int Control_Deside()
+	{
+		//控制方式偵測
+		if (ReadFile ("C:\\Player_Setting.txt", 1) == "Player Control=Mouse And KeyBord")
+		{
+			//滑鼠+鍵盤控制方式
+			Debug.Log("滑鼠+鍵盤控制方式");
+			Control = 1;
+		}
+		else if (ReadFile ("C:\\Player_Setting.txt", 1) == "Player Control=")
+		{
+			//控制方式未設定
+			Debug.Log("控制方式未設定");
+			Control = 0;
+		}
+		else if (ReadFile ("C:\\Player_Setting.txt", 1) == "Player Control=Rocker And headset")
+		{
+			//搖桿+耳機控制方式
+			Debug.Log("搖桿+耳機控制方式");
+			Control = 2;
+		}
+		return Control;
+	}
+
+
+	public int Sound_Str_Content() //劇本內容判斷
+	{
+		/*去讀取文字檔的第一行
+				判斷裡面有沒有內容
+					有內容就直接進入場景
+						沒內容的話進入可編輯版本*/
+		if (ReadFile("C:\\Sound_Str.txt",1)=="")
+		{
+			//文字檔沒有內容
+			Debug.Log("劇本檔沒有內容");
+			int_Sound_Str=0;
+		}
+		else
+		{
+			//文字檔有內容
+			Debug.Log("劇本檔有內容");
+			int_Sound_Str=1;
+		}
+		return int_Sound_Str;
+	}
+
+	public int Scenes_Deside() //場景樣式選擇
+	{
+		//場景樣式 旅館場景or餐廳場景
+		if (ReadFile ("C:\\Player_Setting.txt", 2) == "Scenes Mode=Restaurant")
+		{
+			//場景樣式 餐廳場景
+			Debug.Log("場景樣式 餐廳場景");
+			int_Scenes_Deside=1;
+		}
+		else if (ReadFile ("C:\\Player_Setting.txt", 2) == "Scenes Mode=")
+		{
+			//尚未選擇場景樣式
+			Debug.Log("尚未選擇場景樣式");
+			int_Scenes_Deside=0;
+		}
+		else if (ReadFile ("C:\\Player_Setting.txt", 2) == "Scenes Mode=Hotel")
+		{
+			//場景樣式 旅館場景
+			Debug.Log("場景樣式 旅館場景");
+			int_Scenes_Deside=2;
+		}
+		return int_Scenes_Deside;
+	}
+	public int Language_Deside() //語言選擇
+	{
+		//場景樣式 旅館場景or餐廳場景
+		if (ReadFile ("C:\\Player_Setting.txt", 3) == "Language=Chinese")
+		{
+			//語言選擇:中文
+			Debug.Log("語言選擇:中文");
+			int_Language_Deside=1;
+		}
+		else if (ReadFile ("C:\\Player_Setting.txt", 3) == "Language=")
+		{
+			//尚未選擇語言
+			Debug.Log("尚未選擇語言");
+			int_Language_Deside=0;
+		}
+		else if (ReadFile ("C:\\Player_Setting.txt", 3) == "Language=German")
+		{
+			//語言選擇:德文
+			Debug.Log("語言選擇:德文");
+			int_Language_Deside=2;
+		}
+		return int_Language_Deside;
+	}
+}
